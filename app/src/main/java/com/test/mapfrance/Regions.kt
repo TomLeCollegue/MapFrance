0c2cf2d2ac64ddf02d1e26565c0f23a1a4e06b03
Tom <tomkubasik.pro@gmail.com>
2021-10-04 17:59:10 +0200

    display map

---
+++ app/src/main/java/com/test/mapfrance/Regions.kt
@@ -0,0 +1,17 @@
+package com.test.mapfrance
+
+enum class Regions {
+    GrandEst,
+    NouvelleAquitaine,
+    AuvergneRhôneAlpes,
+    BourgogneFrancheComté,
+    Bretagne,
+    CentreValdeLoire,
+    Corse,
+    IleDeFrance,
+    Occitanie,
+    HautDeFrance,
+    Normandie,
+    PaysDeLaLoire,
+    ProvenceAlpesCoteDAzur,
+}
